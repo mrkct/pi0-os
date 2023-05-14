847144c596bb7c48f385dd97c54bf821fe0d0c2c
Marco Cutecchia <marco.cutecchia@outlook.it>
2023-05-14 15:09:22 +0200

    kern: Add header with some useful size constants

---
+++ kernel/sizes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stddef.h>
+
+static constexpr size_t _1KB = 1024;
+static constexpr size_t _4KB = 4 * 1024;
+static constexpr size_t _16KB = 16 * 1024;
+static constexpr size_t _1MB = 1024 * 1024;
