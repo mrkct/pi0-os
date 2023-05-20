740556e819bee2701f5e805cff9b73a35c4bbd34
Marco Cutecchia <marco.cutecchia@outlook.it>
2023-05-20 16:15:54 +0200

    libs: Add memcpy to kernel's libc stubs

--- kernel/lib/libc/string.cpp
+++ kernel/lib/libc/string.cpp
@@ -7,3 +7,12 @@ extern "C" void* memset(void* s, int c, size_t n)
         *p++ = (unsigned char)c;
     return s;
 }
+
+extern "C" void* memcpy(void* dest, void const* src, size_t n)
+{
+    unsigned char* d = (unsigned char*)dest;
+    unsigned char const* s = (unsigned char const*)src;
+    while (n--)
+        *d++ = *s++;
+    return dest;
+}
--- kernel/lib/libc/string.h
+++ kernel/lib/libc/string.h
@@ -3,3 +3,5 @@
 #include <stddef.h>
 
 extern "C" void* memset(void* s, int c, size_t n);
+
+extern "C" void* memcpy(void* dest, void const* src, size_t n);
