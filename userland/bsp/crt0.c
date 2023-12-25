15315526c80396453c9942962b8854394a7c1e34
Marco Cutecchia <marco.cutecchia@outlook.it>
2023-12-25 19:55:42 +0100

    userland: Use self-built Newlib with debugging symbols

--- userland/bsp/crt0.c
+++ userland/bsp/crt0.c
@@ -2,11 +2,19 @@ extern void __libc_init_array(void);
 extern int main(int argc, const char *argv[]);
 extern void exit(int status);
 
-void _my_start(void)
+void _init()
+{
+}
+
+void _fini()
+{
+}
+
+void _start(void)
 {
     __libc_init_array();
 
-    const char *argv[] = {"hello.exe", "\0"};
-    int result = main(1, argv);
+    const char *argv[] = {"\0"};
+    int result = main(0, argv);
     exit(result);
 }
--- userland/toolchain.mk
+++ userland/toolchain.mk
@@ -8,15 +8,19 @@ CFLAGS := -g -Wall -Wextra \
 	-fno-unwind-tables \
 	-T $(BSP_HERE)/bsp/linker.ld \
 	-mcpu=arm1176jzf-s \
+	-nostdlib \
 	--freestanding \
 	-static \
 	-fno-pic \
 	-I$(BSP_HERE)/../include \
 	-I$(BSP_HERE)/libs \
-	-L$(BSP_HERE)/libs
+	-L$(BSP_HERE)/libs \
+	-L$(BSP_HERE)/bsp/newlib \
 
 include $(BSP_HERE)/bsp/objects.mk
 BSP_OBJECTS:=$(addprefix $(BSP_HERE)/bsp/, $(BSP_OBJECTS))
+
+BSP_LIBS := -lc -lg -lgcc -lm
 
 .PHONY: bsp
 
