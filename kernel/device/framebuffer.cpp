e47dc3c1f5041e109d6a4f5d7b2e6565c68821b1
Marco Cutecchia <marco.cutecchia@outlook.it>
2024-03-31 15:34:23 +0200

    Faster blit_to_framebuffer impl

--- kernel/device/framebuffer.cpp
+++ kernel/device/framebuffer.cpp
@@ -1,5 +1,6 @@
 #include <kernel/device/framebuffer.h>
 #include <kernel/lib/math.h>
+#include <kernel/lib/string.h>
 
 
 namespace kernel {
@@ -14,25 +15,29 @@ Framebuffer& get_main_framebuffer()
 
 void blit_to_main_framebuffer(uint32_t *data, int32_t x, int32_t y, uint32_t width, uint32_t height)
 {
-    auto x1 = max<int32_t>(0, x);
-    auto y1 = max<int32_t>(0, y);
-    auto x2 = min<int32_t>(g_main_framebuffer.width, x + width);
-    auto y2 = min<int32_t>(g_main_framebuffer.height, y + height);
+    auto target_x1 = max<int32_t>(0, x);
+    auto target_y1 = max<int32_t>(0, y);
+    auto target_x2 = min<int32_t>(g_main_framebuffer.width, x + width);
+    auto target_y2 = min<int32_t>(g_main_framebuffer.height, y + height);
 
-    uint32_t src_width = x2 - x1;
-    uint32_t src_height = y2 - y1;
+    auto source_x1 = abs(min<int32_t>(x, 0));
+    auto source_y1 = abs(min<int32_t>(y, 0));
+    auto source_x2 = min<int32_t>(x + width, g_main_framebuffer.width) - x;
+    auto source_y2 = min<int32_t>(y + height, g_main_framebuffer.height) - y;
 
-    if (src_width <= 0 || src_height <= 0)
+    if (target_x2 - target_x1 <= 0 || target_y2 - target_y1 <= 0)
         return;
 
-    for (uint32_t j = 0; j < src_height; ++j) {
-        for (uint32_t i = 0; i < src_width; ++i) {
-            uint32_t dest_index = (y1 + j) * g_main_framebuffer.width + (x1 + i);
-            uint32_t source_index = (y1 - y + j) * width + (x1 - x + i);
-            g_main_framebuffer.address[dest_index] = data[source_index];
-        }
+    uint32_t *src = &data[source_y1 * height + source_x1];
+    uint32_t *dst = (uint32_t*)&(((uint8_t*)g_main_framebuffer.address)[target_y1 * g_main_framebuffer.pitch + target_x1]);
+
+    auto lines_to_copy = target_y2 - target_y1;
+    auto bytes_in_a_line = (target_x2 - target_x1) * sizeof(uint32_t);
+    for (; lines_to_copy > 0; lines_to_copy--) {
+        memcpy(dst, src, bytes_in_a_line);
+        src += width;
+        dst = (uint32_t*) ((uint8_t*) dst + g_main_framebuffer.pitch);
     }
 }
-
 
 }
