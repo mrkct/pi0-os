d5d3c8d214e561d1142ec48b27e9a42300735757
Marco Cutecchia <marco.cutecchia@outlook.it>
2025-01-18 19:56:25 +0100

    bring back cowsay

--- userland/crash/main.c
+++ userland/crash/main.c
@@ -8,8 +8,10 @@ int main(int argc, char **argv)
 
     printf("Goodbye :(\n");
 
-    int *x = (int*) 0xbffffffc;
+    int *x = (int*) 0xfffffffc;
     *x = 1234;
+
+    printf("Uh oh, this should not be printed\n");
 
     return 0;
 }
